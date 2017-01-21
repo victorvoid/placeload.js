ed4fd4e29b4a2af23cb2f0cb3fdea115995215e3
Victor Igor <victor.0w3@hotmail.com>
2017-01-21 12:27:55 -0200

    support marginTop available

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 var placeload = function(){
 	//default options
-	var options = {
+	var dataDefault = {
 		backgroundColor: '',
-		animationDelay: 300
+		animationDelay: 300,
+		width: '',
+		height: '',
+		marginTop: ''
 	};
-
-	function setOptions (customOptions) {
-    // Custom options
-    for (var key in customOptions) {
-      options[key] = customOptions[key];
-    }
-  }
 
 	function getAllElementsWithAttribute(attribute)
 	{
@@ -28,6 +24,11 @@ var placeload = function(){
 	}
 
 	function draw(dataComponent){
+
+		for (var key in dataComponent) {
+			dataDefault[key] = dataComponent[key];
+		}
+
 		puts 'draw...';
 
 		var animateContent = document.querySelector('.animated-background');
@@ -36,13 +37,15 @@ var placeload = function(){
 		var sideSizeX = (parent.offsetWidth - node.offsetWidth)/2;
 		var sideSizeY = (parent.offsetHeight - node.offsetHeight)/2;
 
+		//helpers /o/
 		var addClass = λ classname -> λ el => { el.className += ' ' + classname; return el};
 		var appendIn = λ el -> λ x -> el.appendChild(x);
 		var size = λ(width, height) -> λ (el) => {
-			el.style.width = width + 'px';
-			el.style.height = height + 'px';
+			el.style.width = width;
+			el.style.height = height;
 			return el;
 		};
+
 
 		var position = λ (obj) -> λ el => {
 			if(obj.top != 'undefined') el.style.top = obj.top + 'px';
@@ -53,31 +56,37 @@ var placeload = function(){
 			return el;
 		};
 
+
 		var heightLastElement = getAllElementsWithAttribute('data-active').map(λ x -> x.offsetHeight)[0];
-
-		var sideLeft = document.createElement('div')
+		var marginTopValue = dataDefault.marginTop.slice(0, dataDefault.marginTop.indexOf('px'));
+		marginTopValue = marginTopValue === '' ? marginTopValue : parseInt(marginTopValue);
+		var marginTopElement = document.createElement('div')
 				|> appendIn(animateContent)
 				|> addClass('background-masker')
-				|> size(dataComponent.width, dataComponent.height)
-				|> position({top: heightLastElement, left: 0})
+				|> size('100%', dataDefault.marginTop)
+				|> position({top: heightLastElement, left: 0});
 
-		var sideRight = document.createElement('div')
+		var sideLeftElement = document.createElement('div')
 				|> appendIn(animateContent)
 				|> addClass('background-masker')
-				|> size(dataComponent.width, dataComponent.height)
-				|> position({top: heightLastElement, right: 0});
+				|> size(dataDefault.width, dataDefault.height)
+				|> position({top: heightLastElement+marginTopValue, left: 0});
 
+		var sideRightElement = document.createElement('div')
+				|> appendIn(animateContent)
+				|> addClass('background-masker')
+				|> size(dataDefault.width, dataDefault.height)
+				|> position({top: heightLastElement+marginTopValue, right: 0});
 
 		getAllElementsWithAttribute('data-active').map(λ x =>{
 			x.removeAttribute('data-active');
 		});
 
-		sideRight.setAttribute("data-active", "true");
+		sideRightElement.setAttribute("data-active", "true");
 	}
 
 	return {
-		draw: draw,
-		setOptions: setOptions
+		draw: draw
 	};
 };
 
