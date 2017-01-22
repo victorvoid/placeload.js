8489118cb269de1e8cb2b83d1c4fc6d15312d62f
Victor Igor <victor.0w3@hotmail.com>
2017-01-21 22:08:25 -0200

    :bug center side

--- src/app.js
+++ src/app.js
@@ -1,19 +1,5 @@
 var placeload = function(){
 	var fullHeight = 0;
-
-	function getAllElementsWithAttribute(attribute) {
-		var matchingElements = [];
-		var allElements = document.getElementsByTagName('*');
-		for (var i = 0, n = allElements.length; i < n; i++)
-		{
-			if (allElements[i].getAttribute(attribute) !== null)
-			{
-					// Element exists with attribute. Add to array.
-				matchingElements.push(allElements[i]);
-			}
-		}
-		return matchingElements;
-	}
 	function draw(dataComponent){
 
 		var dataDefault = {
@@ -31,19 +17,30 @@ var placeload = function(){
 
 		puts 'draw...';
 
-		var animateContent = document.querySelector('.animated-background');
-		var animateContentX = animateContent.offsetWidth;
-		//helpers /o/
-		var addClass = λ classname -> λ el => { el.className += ' ' + classname; return el};
-		var appendIn = λ el -> λ x -> el.appendChild(x);
-		var removeUnit = λ st -> st.slice(0, st.indexOf('px'));
-		var sideInCenterSizeX = (animateContentX - parseInt(dataDefault.width |> removeUnit))/2;
-		var sideSizeX = (animateContentX - parseInt(dataDefault.width |> removeUnit));
+		var animateContentEl = document.querySelector('.animated-background');
+		var animateContentX = animateContentEl.offsetWidth;
+		//helpers λ -> λ -> n
+		var addClass  /* |o\ */= λ classname -> λ el => {
+			el.className += ' ' + classname;
+			return el;
+		};
 		var size = λ(width, height) -> λ (el) => {
 			el.style.width = width;
 			el.style.height = height;
 			return el;
 		};
+		var appendIn           = λ el -> λ x -> el.appendChild(x);
+		var removeUnit         = λ st -> st.slice(0, st.indexOf('px'));
+
+		var marginTopValue     = dataDefault.marginTop.slice(0, dataDefault.marginTop.indexOf('px'));
+		marginTopValue         = marginTopValue === '' ? 0 : parseInt(marginTopValue)
+		var topPositionElement = fullHeight + marginTopValue;
+		var sideInCenterSizeX  = (animateContentX - parseInt(dataDefault.width
+															|> removeUnit))/2;
+		var sideSizeX          = (animateContentX - parseInt(dataDefault.width
+															|> removeUnit));
+
+		var widthElement       = dataDefault.center ? sideInCenterSizeX: 0;
 		var position = λ (obj) -> λ el => {
 			if(isNotUndef$(obj.top))    el.style.top    = obj.top    + 'px';
 			if(isNotUndef$(obj.right))  el.style.right  = obj.right  + 'px';
@@ -51,40 +48,27 @@ var placeload = function(){
 			if(isNotUndef$(obj.left))   el.style.left   = obj.left   + 'px';
 			return el;
 		};
-
-		var heightLastElement = getAllElementsWithAttribute('data-active').map(λ x -> x.offsetHeight)[0];
-		var marginTopValue = dataDefault.marginTop.slice(0, dataDefault.marginTop.indexOf('px'));
-		marginTopValue = marginTopValue === '' ? 0 : parseInt(marginTopValue)
-		console.log(sideInCenterSizeX)
-		var topPositionElement = fullHeight + marginTopValue;
-		var widthElement = dataDefault.center ? sideInCenterSizeX: 0;
 		var marginTopElement = document.createElement('div')
-				|> appendIn(animateContent)
+				|> appendIn(animateContentEl)
 				|> addClass('background-masker')
 				|> size('100%', dataDefault.marginTop)
 				|> position({top: fullHeight, left: 0});
 
 		var sideLeftElement = document.createElement('div')
-				|> appendIn(animateContent)
+				|> appendIn(animateContentEl)
 				|> addClass('background-masker')
 				|> size(widthElement+'px', dataDefault.height)
 				|> position({top: topPositionElement, left: 0});
 
 		var sideRightElement = document.createElement('div')
-				|> appendIn(animateContent)
+				|> appendIn(animateContentEl)
 				|> addClass('background-masker')
 				|> size(dataDefault.center ? widthElement+'px': sideSizeX+'px', dataDefault.height)
 				|> position({top: topPositionElement, right: 0});
 
-		getAllElementsWithAttribute('data-active').map(λ x =>{
-			x.removeAttribute('data-active');
-		});
-
 		fullHeight += parseInt(dataDefault.height |> removeUnit) + marginTopValue;
-		animateContent.style.height = fullHeight + 'px';
-		sideRightElement.setAttribute("data-active", "true");
+		animateContentEl.style.height = fullHeight + 'px';
 	}
-
 	return {
 		draw: draw
 	};
@@ -92,7 +76,7 @@ var placeload = function(){
 // Export
 if (typeof window !== 'undefined' && window) {
   if (typeof module === 'object' && module.exports) {
-    // Node.js module.exports = placeload();
+    	module.exports = placeload();
   } else {
     // Browser
     window.placeload = placeload();
