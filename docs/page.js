8041d05eb5b638dc741a56f9770523ccfe9c3b2a
Victor Igor <victor.0w3@hotmail.com>
2017-01-24 12:05:36 -0200

    layout docs changed

--- docs/page.js
+++ docs/page.js
@@ -1,8 +1,8 @@
 var app = (function(){
-    var navigation = document.querySelector('#navigation__menu');
+    var navigation = document.querySelector('#header');
     window.addEventListener("scroll", function(event) {
       var top = this.scrollY;
-      if(top > 900){
+      if(top > 600){
         if(navigation.className !== 'nav-fix') navigation.className = 'nav-fix'
       }else{
         navigation.className = ' '
