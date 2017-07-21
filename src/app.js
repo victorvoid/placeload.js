af86cdfaf8c0595719b5bcc682e9c2dccd0046df
Victor Igor <victor.0w3@hotmail.com>
2017-07-21 12:59:07 -0300

    :art: fixed IO monad in map to features api

--- src/app.js
+++ src/app.js
@@ -6,34 +6,37 @@ const configIO = (_IO) => {
   })
 }
 
-const createElemFolk = (IO, element, prop, value) => {
-  return IO.map(elements => {
-    return elements.map(elem => {
-      element.style[prop] = value
-      elem.placeload.appendChild(element)
-      return elem
-    })
+// createElemFolk :: Either -> Node(DOM) -> prop -> value -> Object(DOM Elements)
+const createElemFolk = (elements, newElement, prop, value) => {
+  return elements.map(elem => {
+    newElement.style[prop] = value
+    elem.placeload.appendChild(newElement)
+    return elem
   })
 }
 
-const elementStyle = (_IO, element) => ({
-  width:  (size) => elementStyle(createElemFolk(_IO, element, 'width', size + 'px'), element),
-  height: (size) => elementStyle(createElemFolk(_IO, element, 'height', size + 'px'), element),
-  _IO
+const elementStyle = (elements, newElement) => ({
+  width:  (size) => elementStyle(createElemFolk(elements, newElement, 'width', size + 'px'), newElement ),
+  height: (size) => elementStyle(createElemFolk(elements, newElement, 'height', size + 'px'), newElement )
 })
 
 // drawIO :: a -> IO -> a(IO)
 const drawIO = (f, _IO, element) => {
-  const newElement = document.createElement('div')
-  newElement.className += ' placeload-masker'
-  return f(elementStyle(_IO, newElement))
+  return _IO.map((elements) => {
+    const newElement = document.createElement('div')
+    newElement.className += ' placeload-masker'
+    f(elementStyle(elements, newElement))
+    return elements
+  })
 }
 
 const place = (_IO) => {
   return ({
     config: () => place(configIO(_IO)),
-    draw: f => place(drawIO(f, _IO)),
-    fold: (err, succ) => _IO._IO.runIO().either(err, succ),
+    line: f => {
+      return place(drawIO(f, _IO))
+    },
+    fold: (err, succ) => _IO.runIO().either(err, succ),
     inspect: () => console.log('IO: ', _IO)
   })
 }
@@ -57,6 +60,6 @@ const Placeload = {
 Placeload
   .$('.user-placeload')
   .config({color: 'red'})
-  .draw((element) => element.width(100).height(100))
+  .line((element) => element.width(100).height(100))
   .fold( (err)=> console.log('error: ', err),
          (right) => console.log('sucess: ', right) )
